3e3f4d5b782e581f2d11520bf1a0333a23ac8546
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2023-12-26 20:22:28 +0100

    feat add styles header

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import { Header } from './components/Header';
 import {Footer} from "./components/Footer";
 import { AppContext, UserContext } from "./contexts/context";
 import { useContext } from "react";
-
+import styles from "./app.module.css"
 function App() {
   const {userLogin}=useContext(UserContext);
   return (
-  <>
+  <div  className={styles.AppContainer}>
    {userLogin&& <Header/>}
     <Outlet/>
     <Footer/>
   
-  </>
+  </div>
   )
 }
 
